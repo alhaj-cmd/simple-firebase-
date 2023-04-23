8a4d2f3259620859f02f2588f65e61f869dc42d1
github_username <you@domain.com>
2023-04-24 00:34:34 +0600

    simple authentication

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,31 +1,69 @@
-import React from 'react';
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import React, { useState } from 'react';
+import { GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
 import app from '../Firebase/FirebaseInit';
 
 
 
 
 const Login = () => {
+    const [user, setUser] = useState(null);
     const auth =getAuth(app)
     console.log(auth)
-    const provider = new GoogleAuthProvider();
+    const googleProvider = new GoogleAuthProvider();
+    const githubProvider = new GithubAuthProvider();
+
 
 
     const handleGoogleSingnIn = () =>{
         // console.log('google signIN')
-        signInWithPopup(auth, provider)
+        signInWithPopup(auth, googleProvider)
     
     .then(result => {
-        const user = result.user;
-        console.log(user);
+        const loginuser = result.user;
+        console.log(loginuser);
+        setUser(loginuser)
     })
     .catch(error => {
         console.log('error', error.message)
     })
 }
+// handle Github signIn
+
+const handleGithubSignIn = ( ) =>{
+    signInWithPopup(auth, githubProvider)
+    .then(result => {
+        const loggedUser = result.user;
+        console.log(loggedUser);
+        setUser(loggedUser);
+    })
+    .catch(error => {
+        console.log(error)
+    })
+}
+
+const handleSignOut = () =>{
+    signOut(auth)
+    .then(result => {
+        console.log(result)
+        setUser(null);
+    })
+    .catch(error => {
+        console.log(error)
+    })
+}
     return (
         <div>
-            <button onClick={handleGoogleSingnIn}>Google login</button>
+          { user ?  <button onClick={handleSignOut}>SignOut</button> :
+            <div>
+                <button onClick={handleGoogleSingnIn}>Google login</button>
+                <button onClick={handleGithubSignIn}>githubSingIn</button>
+                </div>
+                }
+           { user && <div>
+                <h3>user : {user.displayName}</h3>
+                <img src={user.photoURL} alt="" />
+                <p>{user.email}</p>
+            </div>}
         </div>
     );
 };
